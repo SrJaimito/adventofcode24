a618672e185c390f280640ae11dbc6c6d3927fc2
SrJaimito <jaime.senors@gmail.com>
2024-12-03 11:12:48 +0100

    Add first solution for Day 2

--- day_2_1/src/main.rs
+++ day_2_1/src/main.rs
@@ -1,3 +1,39 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let num_safe = reader.lines()
+        .filter(|line| {
+            let signals: Vec<i32> = line.as_ref().unwrap().split_whitespace()
+                .map(|word| {
+                    word.parse::<i32>().unwrap()
+                })
+                .collect();
+
+            let increasing = signals[0] < signals[1];
+            let mut is_safe = true;
+
+            for i in 0..(signals.len() - 1) {
+                if increasing {
+                    if (signals[i + 1] > signals[i] + 3) || (signals[i + 1] < signals[i] + 1) {
+                        is_safe = false;
+                        break;
+                    }
+                } else {
+                    if (signals[i + 1] < signals[i] - 3) || (signals[i + 1] > signals[i] - 1) {
+                        is_safe = false;
+                        break;
+                    }
+                }
+            }
+
+            is_safe
+        })
+        .count();
+
+    println!("{}", num_safe);
 }
+
